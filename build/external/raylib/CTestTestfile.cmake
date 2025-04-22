645f7ff782a8d7e9bebabb2ca963092c622ec05d
sibjor <sixteen610@gmail.com>
2025-04-22 10:56:34 +0200

    Raylib installed

---
+++ build/external/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sixten/Dokument/ToyEngine/external/raylib
+# Build directory: /home/sixten/Dokument/ToyEngine/build/external/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "raylib.h"
+
+int main() {
+    InitWindow(800, 600, "Raylib i VS Code!");
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hej från Raylib!", 200, 200, 20, DARKGRAY);
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
