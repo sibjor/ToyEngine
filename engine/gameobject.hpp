2111338cb0c77e5ce35bbd34624dfb68568c7d17
sibjor <sixteen610@gmail.com>
2025-04-23 12:00:38 +0200

    Default event system, space implemented so far

--- engine/gameobject.hpp
+++ engine/gameobject.hpp
@@ -3,11 +3,51 @@
 class GameObject
 {
     public:
-        GameObject()
-        {
-        };
-
+        GameObject();
+        // Variant of a constructor
+        GameObject(int id, const std::string& name){
+            this->id = id;
+            this->name = name;
+            this->active = true;
+            this->parent = nullptr;
+        }
+        // Another variant of a constructor
+        GameObject(const std::string& name){
+            this->name = name;
+            this->active = true;
+            this->parent = nullptr;
+        }
+        // Destructor
         ~GameObject();
+
+        // Function to add a component
+        template<typename T>
+        T* AddComponent()
+        {
+            T* component = new T();
+            components.push_back(component);
+            return component;
+        }
+        // Function to remove a component
+        template<typename T>
+        void RemoveComponent()
+        {
+            for (auto it = components.begin(); it != components.end(); ++it)
+            {
+                if (dynamic_cast<T*>(*it))
+                {
+                    delete *it;
+                    components.erase(it);
+                    break;
+                }
+            }
+        }
+        // Function to get all children
+        std::vector<GameObject*> GetChildren()
+        {
+            return children;
+        }
+        
 
     private:
         int id;
---
+++ engine/server.hpp
@@ -0,0 +1,13 @@
+#include "central.h"
+
+class Server
+{
+public:
+    Server();
+    ~Server();
+
+    void Init();
+    void Update();
+    void Render();
+    void Shutdown();
+}
